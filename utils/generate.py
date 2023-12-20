5132b5ec8c21f9ddc510c2ca31ef9f32c1243624
AGBonnet <abonnet04@gmail.com>
2023-12-20 17:56:58 +0100

    Fixed import

--- utils/generate.py
+++ utils/generate.py
@@ -10,8 +10,9 @@ import argparse
 import shutil
 import pandas as pd
 
-from chat import generate_answers, build_messages
-from inference import load_file
+from chat import *
+from inference import *
+
 
 def make_prompts(instruction, note=None, dialogue=None, template=None):
     '''
