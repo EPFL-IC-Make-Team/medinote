e391ea448f215cec7f7c00b41a54f81f55ba1454
root <root@gpu005.rcp.epfl.ch>
2023-12-26 17:27:14 +0100

    added inference

--- utils/run_inference.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 inference.py \
-    --model_name meditron-7b-summarizer \
-    --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-summarizer/hf_checkpoint/ \
-    --data_path /pure-mlo-scratch/make_project/data/inference/meditron-7b-summarizer.jsonl \
-    --output_path data/meditron-7b-summarizer.jsonl
