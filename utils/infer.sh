403ffb32f3896a4970733730bc9de672eda71757
AGBonnet <abonnet04@gmail.com>
2023-12-28 15:35:12 -0500

    debugging

--- utils/infer.sh
+++ utils/infer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Accept an argument to ./infer.sh --model_name and redirect to the correct command
-if [ "$1" == "meditron-7b-summarizer" || "$1" == "all" ]; then
+if [ "$1" == "meditron-7b-summarizer" ] || [ "$1" == "all" ]; then
     python3 utils/inference.py \
         --model_name meditron-7b-summarizer \
         --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-summarizer/hf_checkpoint/ \
@@ -12,7 +12,7 @@ if [ "$1" == "meditron-7b-summarizer" || "$1" == "all" ]; then
         --template_path /pure-mlo-scratch/make_project/ClinicalNotes/generation/templates/template.json \
         --verbose
 fi     
-if [ "$1" == "meditron-7b-generator" || "$1" == "all" ]; then
+if [ "$1" == "meditron-7b-generator"  ] || [  "$1" == "all" ]; then
     python3 utils/inference.py \
         --model_name meditron-7b-generator \
         --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-generator/hf_checkpoint/ \
@@ -22,7 +22,7 @@ if [ "$1" == "meditron-7b-generator" || "$1" == "all" ]; then
         --mode generator  \
         --verbose
 fi
-if [ "$1" == "meditron-7b-direct" || "$1" == "all" ]; then
+if [ "$1" == "meditron-7b-direct"  ] || [  "$1" == "all" ]; then
     python3 utils/inference.py \
         --model_name meditron-7b-direct \
         --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-generator/hf_checkpoint/ \
@@ -32,7 +32,7 @@ if [ "$1" == "meditron-7b-direct" || "$1" == "all" ]; then
         --mode direct \
         --verbose
 fi
-if [ "$1" == "meditron-13b-summarizer" || "$1" == "all" ]; then
+if [ "$1" == "meditron-13b-summarizer"  ] || [  "$1" == "all" ]; then
     python3 utils/inference.py \
         --model_name meditron-13b-summarizer \
         --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-13b-summarizer/hf_checkpoint/ \
@@ -43,7 +43,7 @@ if [ "$1" == "meditron-13b-summarizer" || "$1" == "all" ]; then
         --template_path /pure-mlo-scratch/make_project/ClinicalNotes/generation/templates/template.json \
         --verbose
 fi
-if [ "$1" == "meditron-13b-generator" || "$1" == "all" ]; then
+if [ "$1" == "meditron-13b-generator"  ] || [  "$1" == "all" ]; then
     python3 utils/inference.py \
         --model_name meditron-13b-generator \
         --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-13b-generator/hf_checkpoint/ \
@@ -53,7 +53,7 @@ if [ "$1" == "meditron-13b-generator" || "$1" == "all" ]; then
         --mode generator \
         --verbose
 fi
-if [ "$1" == "meditron-13b-direct" || "$1" == "all" ]; then
+if [ "$1" == "meditron-13b-direct"  ] || [  "$1" == "all" ]; then
     python3 utils/inference.py \
         --model_name meditron-13b-direct \
         --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-13b-generator/hf_checkpoint/ \
