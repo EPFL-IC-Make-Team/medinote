2a23d2a61304ca095e1c81f98d6045be7c602abc
AGBonnet <abonnet04@gmail.com>
2024-01-04 12:06:56 -0500

    new checkpoint

--- utils/infer.sh
+++ utils/infer.sh
@@ -9,7 +9,7 @@ VERBOSE=0
 if [ "$1" == "meditron-7b-summarizer" ] || [ "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-7b-summarizer \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-summarizer/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-summarizer/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-7b-summarizer.jsonl \
         --num_samples $NUM_SAMPLES \
@@ -20,7 +20,7 @@ fi
 if [ "$1" == "meditron-7b-generator"  ] || [  "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-7b-generator \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-generator/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-generator/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-7b-summarizer.jsonl \
         --num_samples $NUM_SAMPLES \
@@ -30,7 +30,7 @@ fi
 if [ "$1" == "meditron-7b-generator-gpt"  ] || [  "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-7b-generator-gpt \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-generator/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-generator/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-7b-generator-gpt.jsonl \
         --num_samples $NUM_SAMPLES \
@@ -40,7 +40,7 @@ fi
 if [ "$1" == "meditron-7b-direct"  ] || [  "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-7b-direct \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-direct/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/meditron-7b-direct/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-7b-direct.jsonl \
         --num_samples $NUM_SAMPLES \
@@ -50,7 +50,7 @@ fi
 if [ "$1" == "meditron-13b-summarizer"  ] || [  "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-13b-summarizer \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-summarizer/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-summarizer/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-13b-summarizer.jsonl \
         --num_samples $NUM_SAMPLES \
@@ -61,7 +61,7 @@ fi
 if [ "$1" == "meditron-13b-generator"  ] || [  "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-13b-generator \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-generator/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-generator/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-13b-generator.jsonl \
         --num_samples $NUM_SAMPLES \
@@ -71,7 +71,7 @@ fi
 if [ "$1" == "meditron-13b-generator-gpt"  ] || [  "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-13b-generator-gpt \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-generator/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-generator/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-13b-generator-gpt.jsonl \
         --num_samples $NUM_SAMPLES \
@@ -81,7 +81,7 @@ fi
 if [ "$1" == "meditron-13b-direct"  ] || [  "$1" == "all" ]; then
     python3 utils/infer.py \
         --model_name meditron-13b-direct \
-        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-direct-trunc/hf_checkpoint/ \
+        --model_path /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-direct-trunc/hf_checkpoint_new/ \
         --input_path $INPUT_PATH \
         --output_path /pure-mlo-scratch/make_project/data/inference/meditron-13b-direct.jsonl \
         --num_samples $NUM_SAMPLES \
